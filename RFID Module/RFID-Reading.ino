26f75baf451c465a11a24daf002684f76cdecea3
ajainPSU <151772981+ajainPSU@users.noreply.github.com>
2024-11-11 22:18:36 -0800

    Add files via upload

---
+++ RFID Module/RFID-Reading.ino
@@ -0,0 +1,46 @@
+/*
+
+  ESP32 (Feather/Wroom)
+  Checks ID's (in Hex) of cards for RFID sensor
+  Author: Alex Jain <ajain@pdx.edu>
+  Date: Nov 11th, 2024
+  Version: 0.1
+
+ */
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN  23  // PIN23 on Huzzah32
+#define RST_PIN 13 // PIN13 on Huzzah32 
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin(); // init SPI bus
+  rfid.PCD_Init(); // init MFRC522
+
+  Serial.println("Tap an RFID/NFC tag on the RFID-RC522 reader"); // Note: Doesn't print, can remove.
+}
+
+void loop() {
+  if (rfid.PICC_IsNewCardPresent()) { // new tag is available
+    if (rfid.PICC_ReadCardSerial()) { // NUID has been readed
+      MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+      Serial.print("RFID/NFC Tag Type: ");
+      Serial.println(rfid.PICC_GetTypeName(piccType));
+
+      // print UID in Serial Monitor in the hex format
+      Serial.print("UID:");
+      for (int i = 0; i < rfid.uid.size; i++) {
+        Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        Serial.print(rfid.uid.uidByte[i], HEX);
+      }
+      Serial.println();
+
+      rfid.PICC_HaltA(); // halt PICC
+      rfid.PCD_StopCrypto1(); // stop encryption on PCD
+    }
+  }
+}
